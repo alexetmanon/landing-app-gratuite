46e2275a88a15f8537821fb7d48bb76f35256b40
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2018-01-08 15:55:05 +0100

    Removes foundation imports

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,16 +1,16 @@
-import $ from 'jquery';
-import whatInput from 'what-input';
+// import $ from 'jquery';
+// import whatInput from 'what-input';
 
-window.$ = $;
+// window.$ = $;
 
-import Foundation from 'foundation-sites';
+// import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
 
 import Countdown from './countdown';
 
-$(document).foundation();
+// $(document).foundation();
 
 let deadline = new Date('2018-02-15T00:00:00+01:00');
 new Countdown('countdown', deadline);
