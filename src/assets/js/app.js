ff0cef1fdc01ec3a09efef33b01e0123c6ff2ebb
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2018-02-27 19:46:08 +0100

    Integration of typeform button at the end of pre-registration

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,9 +8,16 @@
 // the line below
 //import './lib/foundation-explicit-pieces';
 
-import Countdown from './countdown';
-
 // $(document).foundation();
 
+import Countdown from './countdown';
+
 let deadline = new Date('2018-03-01T00:00:00+01:00');
-new Countdown('countdown', deadline);
+let remaningTime = Date.parse(deadline) - Date.parse(new Date());
+
+if (remaningTime > 0) {
+  document.getElementById('pre-registration').style.display = 'block';
+  new Countdown('countdown', deadline);
+} else {
+  document.getElementById('registration').style.display = 'block';;
+}
